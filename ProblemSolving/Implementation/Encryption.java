ede1eaf7e0b238ac237621955b0aa1a52cb6f381
Viet Dung Nguyen <nguyvi01@gettysburg.edu>
2018-09-03 10:45:22 -0400

    abc

--- ProblemSolving/Implementation/Encryption.java
+++ ProblemSolving/Implementation/Encryption.java
@@ -1,25 +1,49 @@
     static String encryption(String s) {
-        String text;
+        String text = "";
         for (int i = 0; i < s.length(); i++){
-            if (!s.subString(i, i+1).equals(" ")){
-                text += s.subString(i, i+1);
+            if (!s.substring(i, i+1).equals(" ")){
+                text += s.substring(i, i+1);
             }
         }
         int row = (int)Math.sqrt(text.length());
         int column = (int)Math.sqrt(text.length()) + 1;
         
-        String[][] value = new Stirng[row][column];
+        //Expand the columns if the sentence doesn't fit in a square matrix
+        column = (s.length() > row*row) ? row+1 : row;
+        
+        //Expand the rows if the sentence still doesn't fit
+        row = (s.length() > row*column) ? row+1 : row;
+        
+        String[][] value = new String[row][column];
         
         int index = 0;
         
-        for (int i = 0; i < row){
-            for (int j = 0; j < column){
-                value[i][j] = s.subStirng(index, index + 1);
+        boolean found = false;
+        
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < column; j++){
+                if (index < s.length()){
+                    value[i][j] = s.substring(index, index + 1);
+                    index ++;
+                }else{
+                    found = false;
+                    break;
+                }
+            }
+            if (found){
+                break;
             }
         }
         
-        String returnVal;
+        String returnVal = "";
         
-        for ()
-        
+        for (int j = 0; j < column; j++){
+            for (int i = 0; i < row; i++){
+                if (value[i][j] != null){
+                    returnVal += value[i][j];
+                }
+            }
+            returnVal += " ";
+        }
+        return returnVal;
     }
