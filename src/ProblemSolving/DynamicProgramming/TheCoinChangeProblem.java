75d85c00c1ff770221a1a0f378d8751c2c7d350d
dewanderelex <nguyvi01@gettysburg.edu>
2018-11-11 17:18:03 -0500

    More Algorithms

--- src/ProblemSolving/DynamicProgramming/TheCoinChangeProblem.java
+++ src/ProblemSolving/DynamicProgramming/TheCoinChangeProblem.java
@@ -1,5 +1,84 @@
 package ProblemSolving.DynamicProgramming;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class TheCoinChangeProblem {
-    
+
+    // Complete the getWays function below.
+    static long getWays(long n, long[] c) {
+        long[] table = new long[ (int) n + 1];
+        table[0] = 1;
+
+        for (long i : c){
+            for (long curr = i; curr <= n; curr ++){
+                table[(int)curr] += table[(int)(curr - i)];
+            }
+        }
+        return table[(int) n ];
+    }
+
+    static long countWays(int S[], int n)
+    {
+        //Time complexity of this function: O(mn)
+        //Space Complexity of this function: O(n)
+
+        // table[i] will be storing the number of solutions
+        // for value i. We need n+1 rows as the table is
+        // constructed in bottom up manner using the base
+        // case (n = 0)
+        long[] table = new long[n+1];
+
+        // Initialize all table values as 0
+        Arrays.fill(table, 0);   //O(n)
+
+        // Base case (If given value is 0)
+        table[0] = 1;
+
+        // Pick all coins one by one and update the table[]
+        // values after the index greater than or equal to
+        // the value of the picked coin
+        for (int i : S) {
+            for (int j = i; j <= n; j++) {
+                table[j] += table[j - i];
+            }
+        }
+        return table[n];
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        long[] c = new long[m];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            long cItem = Long.parseLong(cItems[i]);
+            c[i] = cItem;
+        }
+
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+        long ways = getWays(n, c);
+
+        System.out.println(ways);
+
+        scanner.close();
+    }
 }
+
